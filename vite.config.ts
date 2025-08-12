eee0b7f930ffca7bbeab891e7116f60920b139b9
Jeet Manjrekar <letsmailjeet@gmail.com>
2025-08-12 17:35:00 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/Personal-Portfolio/', // This line is added to fix the blank page issue
   server: {
     host: "::",
     port: 8080,
